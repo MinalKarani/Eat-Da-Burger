4f4758a630e2505cb70797e23e011b4c7685d5c2
Minal Karani <karani.minal@gmail.com>
2019-02-27 11:42:15 -0500

    burger

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,19 @@
 var mysql = require("mysql");
 require("dotenv").config();
 
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.password,
-  database: "burgers_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: process.env.password,
+        database: 'burgers_db'
+    })
+};
 
 module.exports = connection;
